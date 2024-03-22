4b2db122b470c9845f0d95392fe368296f476a02
cox101 <cox2659@gmail.com>
2024-03-22 13:33:41 -0700

    Initial commit

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
-#Kills a process named killmenow
-
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
+#Kills a process named killmenow 
+`
+exec { 'kill-killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin';
 }
-
