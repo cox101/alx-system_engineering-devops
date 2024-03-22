ebabf8b11c0fa4c364f5ab23bbe698b7c5e25863
cox101 <cox2659@gmail.com>
2024-03-22 13:06:36 -0700

    Initial commit'

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
-# install puppet-lint
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+#Kills a process named killmenow
+
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
+}
+
