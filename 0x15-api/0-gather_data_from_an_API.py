b820d61affe94fdeff2ad13b1f6e05cace77fa6c
cox101 <coxb2659@gmail.com>
2024-04-22 17:38:50 -0700

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Script to fetch employee TODO list progress from a REST API.
+"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def fetch_employee_todo_progress(employee_id):
+    """
+    Fetches employee TODO list progress from the given API.
+    """
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+    response = requests.get(url)
+    todos = response.json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    if not todos:
+        print("No data found for employee ID:", employee_id)
+        return
+
+    completed_tasks = [todo for todo in todos if todo['completed']]
+    total_tasks = len(todos)
+    completed_count = len(completed_tasks)
+    employee_name = todos[0]['username']
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_count, total_tasks))
+    for task in completed_tasks:
+        print("\t", task['title'])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    fetch_employee_todo_progress(employee_id)
+
