ca37cbb60a0bc883472690a1bf1cb4792b0abce6
cox101 <coxb2659@gmail.com>
2024-04-22 17:06:07 -0700

    Initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,33 +7,18 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python3 employee_tasks.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
     main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    todo_url = f"{main_url}/users/{employee_id}/todos"
-    name_url = f"{main_url}/users/{employee_id}"
-
-    try:
-        todo_result = requests.get(todo_url).json()
-        name_result = requests.get(name_url).json()
-
-        todo_num = len(todo_result)
-        todo_complete = sum(1 for todo in todo_result if todo.get("completed"))
-        name = name_result.get("name")
-
-        print(f"Employee {name} has completed {todo_complete}/{todo_num} tasks:")
-        for todo in todo_result:
-            if todo.get("completed"):
-                print(f"\t{todo.get('title')}")
-
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-        sys.exit(1)
-    except (KeyError, TypeError) as e:
-        print(f"Error processing data: {e}")
-        sys.exit(1)
-
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
