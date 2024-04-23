b2974e96a9f0e65095bc56027ec82362fde329c7
cox101 <coxb2659@gmail.com>
2024-04-22 17:44:09 -0700

    initial commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,32 +3,22 @@
 Uses the JSON placeholder api to query data about an employee
 """
 
-import requests
-import sys
+from requests import get
+from sys import argv
 
-def get_employee_todo_progress(employee_id):
-    api_url = f"https://api.example.com/employees/{employee_id}/todos"
+if __name__ == '__main__':
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
+    name_result = get(name_url).json()
 
-    try:
-        response = requests.get(api_url)
-        response_data = response.json()
-
-        if "error" in response_data:
-            print(f"Error: {response_data['error']}")
-            return
-
-        employee_name = response_data.get("employee_name", "Unknown Employee")
-        done_tasks = response_data.get("done_tasks", 0)
-        total_tasks = response_data.get("total_tasks", 0)
-
-        print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
-        for task_title in response_data.get("completed_task_titles", []):
-            print(f"\t{task_title}")
-
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-
-if __name__ == "__main__":
-    employee_id = int(input("Enter the employee ID: "))
-    get_employee_todo_progress(employee_id)
-
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
+                         if todo.get("completed")])
+    name = name_result.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
