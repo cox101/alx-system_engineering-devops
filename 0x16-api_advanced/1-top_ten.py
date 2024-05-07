804cafc4c35e358a345dea4280df22f33de50798
cox101 <coxb2659@gmail.com>
2024-05-07 14:23:16 -0700

    Commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,23 +2,26 @@
 """function that queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit."""
 
+import requests
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    data = response.json()
+    posts = data['data']['children']
+
+    if not posts:
+        print(None)
+        return
+
+    print("Top 10 hot posts in r/" + subreddit + ":\n")
+    for post in posts:
+        print(post['data']['title'])
